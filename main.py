3e0d28826618b85dbccec9972af6c9dfe80706d2
Nick Marks <nmarks@anl.gov>
2026-07-16 17:40:06 -0500

    need to use video0, documenting script very clearly now that works

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def get_tcp_offset():
     x = caget("bcur:Control:TCPOffset_X")
     y = caget("bcur:Control:TCPOffset_Y")
     z = caget("bcur:Control:TCPOffset_Z")
-    rx = caget("bcur:Control:TCPOffset_Roll")
-    ry = caget("bcur:Control:TCPOffset_Pitch")
-    rz = caget("bcur:Control:TCPOffset_Yaw")
-    offset = [x/1000.0, y/1000.0, z/1000.0, rx*(np.pi/180.0), ry*(np.pi/180.0), rz*(np.pi/180.0)]
+    rx = caget("bcur:Control:TCPOffset_Rx")
+    ry = caget("bcur:Control:TCPOffset_Ry")
+    rz = caget("bcur:Control:TCPOffset_Rz")
+    offset = [x/1000.0, y/1000.0, z/1000.0, rx, ry, rz]
     print(f"offset = {offset}")
     return offset
 
@@ -21,15 +21,12 @@ def get_tcp_pose():
     pose = caget("bcur:Receive:ActualTCPPose")
     pose[0] /= 1000.0
     pose[1] /= 1000.0
-    pose[2] /= (1000.0)
-    pose[3] *= np.pi/180.0
-    pose[4] *= np.pi/180.0
-    pose[5] *= np.pi/180.0
+    pose[2] /= 1000.0
     print(f"robot pose = {pose}")
     return pose
 
 def main():
-    cam = WristCamera()
+    cam = WristCamera(device_index=0)
     time.sleep(1)
 
     # Locate our sample by tag ID
@@ -47,31 +44,30 @@ def main():
         if not tag:
             return
 
-    print(f"{tag['tf']}")
 
+    # tag in camera frame
     g_ca = RigidTransform.from_matrix(tag["tf"])
 
+    # TCP in world frame
     g_wt = tf.pose_to_tf(get_tcp_pose())
+
+    # TCP in flange frame
     g_ft = tf.pose_to_tf(get_tcp_offset())
+
+    # flange in world frame
     g_wf = g_wt * g_ft.inv()
+
+    # camera in world frame
     g_wc = g_wf * tf.g_fc
-    #  g_wf = tf.pose_to_tf(get_tcp_pose())
-    #  g_wc = g_wf * tf.g_fc
 
-    g_flip = RigidTransform.from_components([0, 0, 0], Rotation.from_rotvec([0, 0, np.pi]))
-    g_wa = g_wc * g_flip * g_ca
-
-    print(f"g_ca:\n{g_ca}")
-    print(f"\ng_wt:\n{g_wt}")
-    print(f"\ng_ft:\n{g_ft}")
-    print(f"\ng_wf:\n{g_wf}")
-    print(f"\ng_wc:\n{g_wc}")
-    print(f"\ng_wa:\n{g_wa}")
+    # And finally, tag in world frame
+    g_wa = g_wc * g_ca
 
     tran, rot = g_wa.as_components()
 
+    print("\nTag pose in robot base frame:")
     x,y,z = tran
-    roll, pitch, yaw = rot.as_euler("xyz", degrees=True);
+    roll, pitch, yaw = rot.as_euler("xyz");
     print(f"(x,y,z) = ({x},{y},{z})")
     print(f"(roll,pitch,yaw) = ({roll},{pitch},{yaw})")
 
